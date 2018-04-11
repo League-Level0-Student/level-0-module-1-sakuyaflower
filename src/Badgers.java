e5c783f6dda6cdd405b8e9e61b0b0122d4770920
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-10 17:54:34 -0700

    badger my ages

---
+++ src/Badgers.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Badgers {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 8; i++) {
+		if(i % 8==3) {
+		System.out.println("Mushroom");
+		}else if(i % 8==7) {
+		System.out.println("Snake");
+	    }else {
+		System.out.println("Badger");
+		}
+		
+		
+			
+		
+		
+	}
+}
+}
--- src/MyAges.java
+++ src/MyAges.java
@@ -5,6 +5,7 @@ public static void main(String[] args)
 {
 for (int i = 0; i < 14; i++) {
 	System.out.println(i);
+	
 }
 }
 }
